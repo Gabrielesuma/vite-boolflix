6bdadd2850e81e32438dbb01b05a680520cb70bc
Gabrielesuma <gabrielesuma139@gmail.com>
2024-04-18 17:34:47 +0200

    milestone 2 3

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,29 +1,57 @@
 <template>
     <div class="card">
-        <img src="..." class="card-img-top" alt="...">
+        <img :src="store.imageUrl + item.poster_path" class="card-img-top" :alt="item.title || item.name">
         <div class="card-body">
-            <h5 class="card-title">{{ item.title || item.name }}</h5>
+            <h4 class="card-title">{{ item.title || item.name }}</h4>
+            <h6 class="card-title">{{ item.original_title || item.original_name }}</h6>
             <p class="card-text">
-                {{ item.original_title || item.original_name }}
+                <div class="flag">
+                    <img :src="imgFlag" :alt="item.original_language + ' flag'">
+                </div>
                 <br>
-                {{ item.original_language }}
-                <br>
-                {{ item.vote_average }}
+                <div class="stars">
+                    <i :class="vote >= n ? 'fa-solid' : 'fa-regular'" class="fa-star" v-for="n in 5"></i>
+                </div>
             </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
     </div>
 </template>
 
 <script>
+import {store} from '../store.js';
     export default {
         name: 'CardComponent',
         props:{
             item: Object
+        },
+        data(){
+            return{
+                store,
+                flags:['de', 'en', 'es', 'fr', 'it']
+            }
+        },
+        computed:{
+            imgFlag() {
+                if(this.flags.includes(this.item.original_language)){
+                    return `/images/${this.item.original_language}.png`;
+                } else {
+                    return '/images/spaceholder.jpg';
+                }
+            },
+            vote(){
+                return Math.ceil(this.item.vote_average / 2);
+            }
         }
     }
 </script>
 
 <style lang="scss" scoped>
-
+    .flag{
+        img{
+            height: 30px;
+        }
+    }
+    .stars{
+        color: gold;
+    }
 </style>
