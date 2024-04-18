f124d7008873a9425f5a5d11666cc655ae087c1d
Gabrielesuma <gabrielesuma139@gmail.com>
2024-04-18 18:13:02 +0200

    hover

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,20 +1,26 @@
 <template>
-    <div class="card">
-        <img :src="store.imageUrl + item.poster_path" class="card-img-top" :alt="item.title || item.name">
-        <div class="card-body">
-            <h4 class="card-title">{{ item.title || item.name }}</h4>
-            <h6 class="card-title">{{ item.original_title || item.original_name }}</h6>
-            <p class="card-text">
-                <div class="flag">
-                    <img :src="imgFlag" :alt="item.original_language + ' flag'">
+    <div class="flip-card">
+        <div class="flip-card-inner">
+            <div class="card flip-card-front">
+                <img :src="store.imageUrl + item.poster_path" class="card-img-top" :alt="item.title || item.name">
+            </div>
+            <div class="card flip-card-back">
+                <div class="card-body">
+                    <h4 class="card-title">{{ item.title || item.name }}</h4>
+                    <h6 class="card-title">{{ item.original_title || item.original_name }}</h6>
+                    <p class="card-text">
+                        <div class="flag">
+                            <img :src="imgFlag" :alt="item.original_language + ' flag'">
+                        </div>
+                        <div class="stars">
+                            <i :class="vote >= n ? 'fa-solid' : 'fa-regular'" class="fa-star" v-for="n in 5"></i>
+                        </div>
+                    </p>
                 </div>
-                <br>
-                <div class="stars">
-                    <i :class="vote >= n ? 'fa-solid' : 'fa-regular'" class="fa-star" v-for="n in 5"></i>
-                </div>
-            </p>
+            </div>
         </div>
     </div>
+    
 </template>
 
 <script>
@@ -53,5 +59,42 @@ import {store} from '../store.js';
     }
     .stars{
         color: gold;
+    }
+    .flip-card{
+        aspect-ratio: 1/1.5;
+        background-color: transparent;
+        perspective: 1000px;
+
+        .flip-card-inner {
+            position: relative;
+            width: 100%;
+            height: 100%;
+            text-align: center;
+            transition: transform 0.8s;
+            transform-style: preserve-3d;
+        }
+
+        &:hover .flip-card-inner{
+            transform: rotateY(180deg);
+        }
+
+        .flip-card-front,
+        .flip-card-back {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            -webkit-backface-visibility: hidden;
+            /* Safari */
+            backface-visibility: hidden;
+        }
+
+        .flip-card-back{
+            padding: 20px;
+            background-color: black;
+            color: white;
+            transform: rotateY('deg180');
+            overflow-x: hidden;
+            overflow-y: auto;
+        }
     }
 </style>
